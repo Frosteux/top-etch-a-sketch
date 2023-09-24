a41eea474ed33b481d08130c11d6025d3e495ece
Frosteux <21323143+Frosteux@users.noreply.github.com>
2023-09-24 17:37:43 -0400

    Based functionality added, drawing exclusively in red.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,8 @@
 const sketchpad = document.querySelector('.mainSketchpad');
-const sketchpadWidth = sketchpad.offsetWidth;
-const sketchpadHeight = sketchpad.offsetHeight;
-const userSelectedSize = '16'
+const sketchpadWidth = sketchpad.clientWidth;
+const sketchpadHeight = sketchpad.clientHeight;
+const userSelectedSize = '48'
+const drawColor = 'red'
 
 // Use the height and width of the sketchpad to determine what size the boxes inside it need to be.
 const innerSketchpadBoxWidth = sketchpadWidth / +userSelectedSize;
@@ -10,12 +11,17 @@ const innerSketchpadBoxHeight = sketchpadHeight / +userSelectedSize;
 // Build boxes based on the available space.
 for(let i=0;i<+userSelectedSize*+userSelectedSize;i++){
     let innerBox = document.createElement('div');
-    innerBox.style.border = '.1px solid blue';
+    innerBox.style.border = '.1px solid white';
     innerBox.style.width = innerSketchpadBoxWidth + 'px';
     innerBox.style.height = innerSketchpadBoxHeight + 'px';
     innerBox.classList.add('sketchPadInside');
+    innerBox.id=i; //id will be used to change a boxes color in another function
     sketchpad.appendChild(innerBox);
 
 }
-console.log(innerSketchpadBoxHeight);
-console.log(innerSketchpadBoxWidth);
+
+document.addEventListener('mousemove', function(e){
+    const selectedBoxID = e.target.id;
+    const selectedBox = document.getElementById(selectedBoxID);
+    selectedBox.style.backgroundColor = drawColor;
+});
